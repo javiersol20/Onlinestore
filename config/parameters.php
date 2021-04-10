1457510ad0cf4fa8e3fce0508f45f5bca8a21330
Javier Solis <Javiersol2000@outlook.com>
2021-04-10 09:29:46 -0600

    Ending code of the online store with its respective modules

---
+++ config/parameters.php
@@ -0,0 +1,5 @@
+<?php
+define("base_url", "http://storeonline.com.devel/storeOnline/");
+define("controller_default", "productoController");
+define("action_default", "index");
+
---
+++ views/layout/footer.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+
+			<!-- PIE DE PÁGINA -->
+			<footer id="footer">
+				<p>Javier Solís &copy; <?= date('Y') ?></p>
+			</footer>
+		</div>
+	</body>
+</html>
